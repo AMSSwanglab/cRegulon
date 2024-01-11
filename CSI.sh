56a750bb07275ea35897abb75638af9f7b214610
Zhanying Feng <89908600+fengzhanying@users.noreply.github.com>
2024-01-10 20:43:58 -0800

    Delete CSI.sh

--- CSI.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-# TF-TF CSI combinatorial network v1.0.0 updated Mar 11th 2023 by Zhanying Feng
-
-sed s/Sample/${1}/g ./scr/CSI.py > CSI.py
-python3 CSI.py;rm CSI.py
-rm -f ${1}_GA.txt ${1}_GE.txt
